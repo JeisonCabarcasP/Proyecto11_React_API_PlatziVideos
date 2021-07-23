92d74fca10f19a8530c5cae754e014e522ee8bcd
Jeison Cabarcas <ancjeison@gmail.com>
2021-07-22 21:51:36 -0500

    correcion de archivo fakeapi y consumo de api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; //useState maneja el estado y useeffect hace RX/TX a las apiso componentes
 import Header from '../components/Header';
 import Search from "../components/Search";
 import Carousel from '../components/Carousel';
@@ -8,31 +8,43 @@ import Footer from '../components/Footer';
 
 import "../assets/styles/App.scss";
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState([]);
 
-    <Categories title="Mi lista" >
-      <Carousel>
-        <CarouselItem/>
-      </Carousel>
-    </Categories >
+  useEffect(() => {
+    fetch("http://localhost:3000/initalState")
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
 
-    <Categories title="Tendencias" >
-      <Carousel>
-        <CarouselItem/>
-      </Carousel>
-    </Categories >
+  console.log(videos)
 
-    <Categories title="Originales" >
-      <Carousel>
-        <CarouselItem/>
-      </Carousel>
-    </Categories >
+  return (
+    <div className="App">
+      <Header />
+      <Search />
 
-    <Footer />
-  </div>
-);
+      <Categories title="Mi lista" >
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories >
+
+      <Categories title="Tendencias" >
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories >
+
+      <Categories title="Originales" >
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories >
+
+      <Footer />
+    </div>
+  )
+};
 
 export default App;
